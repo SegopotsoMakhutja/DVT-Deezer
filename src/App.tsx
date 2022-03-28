c1cc43af0068d0fa3e4bd4fd0abe3f10e6d59d85
Segopotso Makhutja <segopotso.makhutja@sovtech.com>
2022-03-28 22:05:53 +0200

    bugfix: add ArtistDataProvider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { theme, ChakraProvider } from "@chakra-ui/react"
 import { Router, Outlet } from "@tanstack/react-location";
-import { location, routes } from "routes/routes";
+import { location, routes } from "./routes/routes";
+import SidebarWithHeader from "components/Sidebar/Sidebar"
 import { HelmetProvider } from 'react-helmet-async';
-import { SidebarWithHeader } from "components/Sidebar";
-
+import { ArtistDataProvider } from "context/ArtistContextProvider";
 // helmet context
 const helmetContext = {};
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <HelmetProvider context={helmetContext}>
-      <Router location={location} routes={routes}>
-        <SidebarWithHeader>
-          <Outlet />
-        </SidebarWithHeader>
-      </Router>
+      <ArtistDataProvider>
+        <Router location={location} routes={routes}>
+          <SidebarWithHeader>
+            <Outlet />
+          </SidebarWithHeader>
+        </Router>
+      </ArtistDataProvider>
     </HelmetProvider>
   </ChakraProvider>
 )
